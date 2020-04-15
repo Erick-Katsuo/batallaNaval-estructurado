3eb3afaa4fba6762665d0e5ad1edf7eaee7e16ca
Katsuo <49467285+Erick-Katsuo@users.noreply.github.com>
2020-04-15 18:01:33 -0300

    Update BatallaNaval.java
    juego terminado segun el ejercicio que paso maxi, recordar que la cantidad de barcos se define con la constante MAX_BARC

--- BatallaNaval.java
+++ BatallaNaval.java
@@ -11,52 +11,95 @@ class BatallaNaval{
     static int[] barco;
     static int[] destruir;
 
+    static final char NO_VISTO = '*';
+    static final char ATINE = '!';
+    static final char NO_BARCO = 'X';
+    static final char VACIO = 'V';
+    static final char BARCO = 'B';
+
     static Scanner scanPos = null;
     static int posRecibida = -1;
 
     static Scanner scanMenu = null;
-    static int opcRecibida = -1;
 
+    static final int SEGUIR = 1;
     static final int SALIR = 0;
     static final int DEF_OPC = -1;
     static int opcionMenu = DEF_OPC;
 
+    static final boolean DEF_GANO = false;
+    static final boolean GANO = true;
+    static boolean gano = DEF_GANO;
 
-    public static void mostrarTablero(){
+    static final int NINGUN_BARCO = 0;
+    static int barcosDestruidos = NINGUN_BARCO;
+
+
+    public static void mostrarTablero(){   
+        System.out.print("\n*** Barcos destruidos: " + barcosDestruidos);     
+        System.out.println(" ***\n\n    [0]  [1]  [2]  [3]  [4]");
         for(int i = 0; i<MAX_FIL ; i++){
             for(int j = 0; j<MAX_COL ; j++){
-                System.out.print(" ["+tablero[i][j]+"] ");
+                if(j==0){
+                    System.out.print("["+i+"] ["+tablero[i][j]+"] ");
+                }else{
+                    System.out.print(" ["+tablero[i][j]+"] ");
+                }
             }
             System.out.println();
         }
+        System.out.println("\n• No lo he visto[*]  • Le atine [!]  • No hay barco [X]");
     }
 
     static void pedirCoordenadas(){
-        for(int i = 0; i<2 ; i++){
+        for(int i = 0; i<MAX_BARC; i++){
             System.out.println("Ingrese pos Fila para el barco nº " + (i+1));
             barco[0] = scanPos.nextInt();        
             System.out.println("Ingrese pos Columna para el barco nº " + (i+1));
             barco[1] = scanPos.nextInt();        
-            barcosEnemigos[barco[0]][barco[1]] = 'B';
+            barcosEnemigos[barco[0]][barco[1]] = BARCO;
         }
     }
 
+    /*Pre: Matriz correctamente inicializada. El valor ingresado debe estar dentro del rango de la matriz
+     *Post: Guarda las coordenadas que ingreso el usuario para intentar destruir un barco
+    */
     static void pedirCoordenadasJugador(){
-        System.out.println("Ingrese coordenadas para intentar destuir un barco:");
-        System.out.print("Fila:");
+        System.out.println("-----------------------------------------------\n");
+        System.out.printf("Ingrese coordenadas para intentar destuir un barco:\n");
+        System.out.printf("Fila:");
         destruir[0] = scanPos.nextInt();
         System.out.print("Columna:");
         destruir[1] = scanPos.nextInt();
     }
 
     static void comprobarDestruccion(){
-        if(barcosEnemigos[destruir[0]][destruir[1]] == 'B'){
-            System.out.println("DESTRUISTE UN BARCO");
-            tablero[destruir[0]][destruir[1]] = '!';
+        System.out.print("*** Resultado: ");
+        if(barcosEnemigos[destruir[0]][destruir[1]] == BARCO){
+            System.out.println("DESTRUISTE UN BARCO ***");
+            tablero[destruir[0]][destruir[1]] = ATINE;
+            barcosEnemigos[destruir[0]][destruir[1]] = VACIO;
+            barcosDestruidos++;
+        }else if(barcosEnemigos[destruir[0]][destruir[1]] == VACIO){
+            System.out.println("Barco actualmente destruido ***");
         }else{
-            tablero[destruir[0]][destruir[1]] = 'X';
-            System.out.println("No hay nada xd");
+            tablero[destruir[0]][destruir[1]] = NO_BARCO;
+            System.out.println("No hay nada xd ***");
         }
+    }
+
+    static void opcionSalir(){
+        if(barcosDestruidos != MAX_BARC){
+            System.out.println("\n\t\t***Menu***");
+            System.out.println("• Seguir jugando [1]");
+            System.out.println("• Me enoje >:c   [0]");
+            opcionMenu = scanMenu.nextInt();
+            System.out.println("-----------------------------------------------\n");
+        }else{
+            gano = GANO;
+            System.out.println("\t\tGANASTE !!! :D");
+        }
+        
     }
 
     static void comenzarJuego(){
@@ -64,17 +107,18 @@ class BatallaNaval{
         scanMenu = new Scanner(System.in);
         pedirCoordenadas();
         mostrarTablero();
-        while(opcionMenu != SALIR){
+        while(opcionMenu != SALIR && !gano){
             pedirCoordenadasJugador();
             comprobarDestruccion();
             mostrarTablero();
+            opcionSalir();
         }
     }
 
     static void llenarTablero(){
         for(int i = 0; i<MAX_FIL ; i++){
             for(int j = 0; j<MAX_COL ; j++){
-                tablero[i][j] = '*';
+                tablero[i][j] = NO_BARCO;
             }
         }
     }
@@ -86,6 +130,5 @@ class BatallaNaval{
         destruir = new int[2];
         llenarTablero();
         comenzarJuego();
-        mostrarTablero();
     }
 }
